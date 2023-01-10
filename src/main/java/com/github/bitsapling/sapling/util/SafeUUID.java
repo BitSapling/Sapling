38ba11fed2551f437316b5455c013a1dd94f1eca
Ghost_chu <ghostchu@111.com>
2023-01-11 01:18:20 +0800

    refactor: Enhance SafeUUID with multiple useful functions

--- src/main/java/com/github/bitsapling/sapling/util/SafeUUID.java
+++ src/main/java/com/github/bitsapling/sapling/util/SafeUUID.java
@@ -1,5 +1,6 @@
 package com.github.bitsapling.sapling.util;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
@@ -15,5 +16,27 @@ public class SafeUUID {
         } catch (IllegalArgumentException e) {
             return null;
         }
+    }
+
+    @NotNull
+    public static UUID nilUniqueId() {
+        return new UUID(0, 0);
+    }
+
+    @NotNull
+    public static String stripDashes(@NotNull String uuid) {
+        return uuid.replaceAll("-", "");
+    }
+
+    @Nullable
+    public static String addDashes(@NotNull String uuid) {
+        if (!isUUID(uuid)) {
+            return null;
+        }
+        return uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20);
+    }
+
+    public static boolean isUUID(@NotNull String str) {
+        return str.matches("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
     }
 }
