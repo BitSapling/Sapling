6ec3b86910d67e3b5484d9745296266bde498498
Ghost_chu <ghostchu@111.com>
2023-01-11 23:31:37 +0800

    refactor: mark fields the nullable properties

--- src/main/java/com/github/bitsapling/sapling/entity/Peer.java
+++ src/main/java/com/github/bitsapling/sapling/entity/Peer.java
@@ -15,32 +15,32 @@ import java.util.UUID;
 public class Peer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "uuid")
+    @Column(name = "uuid", nullable = false)
     private UUID uuid;
-    @Column(name = "ip")
+    @Column(name = "ip", nullable = false)
     private String ip;
-    @Column(name = "port")
+    @Column(name = "port", nullable = false)
     private int port;
-    @Column(name = "info_hash")
+    @Column(name = "info_hash", nullable = false)
     private String infoHash;
-    @Column(name = "peer_id")
+    @Column(name = "peer_id", nullable = false)
     private String peerId;
-    @Column(name = "peer_id_hash")
+    @Column(name = "peer_id_hash", nullable = false)
     private String peerIdHash;
-    @Column(name = "user_agent")
+    @Column(name = "user_agent", nullable = false)
     private String userAgent;
-    @Column(name = "uploaded")
+    @Column(name = "uploaded", nullable = false)
     private long uploaded;
-    @Column(name = "downloaded")
+    @Column(name = "downloaded", nullable = false)
     private long downloaded;
-    @Column(name = "seeder")
+    @Column(name = "seeder", nullable = false)
     private boolean seeder;
-    @Column(name = "torrent_id")
+    @Column(name = "torrent_id", nullable = false)
     private long torrentId;
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private long userId;
-    @Column(name = "update_at")
+    @Column(name = "update_at", nullable = false)
     private long updateAt;
-    @Column(name = "connectable")
-    private boolean connectable;
+    @Column(name = "connectable", nullable = false)
+    private int connectable;
 }
