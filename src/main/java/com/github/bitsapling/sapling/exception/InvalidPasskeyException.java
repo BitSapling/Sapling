06bdfa18ddc425e01fe5352deba716036e61e044
Ghost_chu <ghostchu@111.com>
2023-01-11 18:16:53 +0800

    refactor: clean up

--- src/main/java/com/github/bitsapling/sapling/exception/InvalidPasskeyException.java
+++
@@ -1,9 +0,0 @@
-package com.github.bitsapling.sapling.exception;
-
-import org.jetbrains.annotations.NotNull;
-
-public class InvalidPasskeyException extends AnnounceException {
-    public InvalidPasskeyException() {
-        super("Given passkey invalid or not registered");
-    }
-}
