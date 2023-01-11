4e3dc15f574435dff2b9edc34dcc7b5fa76b5247
Ghost_chu <ghostchu@111.com>
2023-01-11 20:54:20 +0800

    feat: EventBus

---
+++ src/main/java/com/github/bitsapling/sapling/event/Cancellable.java
@@ -0,0 +1,6 @@
+package com.github.bitsapling.sapling.event;
+
+public interface Cancellable {
+    boolean isCancelled();
+    void setCancelled();
+}
---
+++ src/main/java/com/github/bitsapling/sapling/event/Event.java
@@ -0,0 +1,5 @@
+package com.github.bitsapling.sapling.event;
+
+public abstract class Event {
+
+}
