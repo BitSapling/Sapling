91ac4af078db9491ff0ff3d0b00485365625ad93
Ghost_chu <ghostchu@111.com>
2023-01-11 18:16:32 +0800

    refactor: Refactor partial AnnounceController

--- src/main/java/com/github/bitsapling/sapling/controller/announce/AnnounceController.java
+++ src/main/java/com/github/bitsapling/sapling/controller/announce/AnnounceController.java
@@ -2,10 +2,11 @@ package com.github.bitsapling.sapling.controller.announce;
 
 import com.github.bitsapling.sapling.exception.AnnounceException;
 import com.github.bitsapling.sapling.exception.InvalidAnnounceException;
-import com.github.bitsapling.sapling.exception.InvalidPasskeyException;
 import com.github.bitsapling.sapling.exception.TrackerException;
 import com.github.bitsapling.sapling.model.BlacklistClient;
 import com.github.bitsapling.sapling.type.AnnounceEventType;
+import com.github.bitsapling.sapling.util.BooleanUtil;
+import com.github.bitsapling.sapling.util.MiscUtil;
 import com.github.bitsapling.sapling.util.SafeUUID;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -42,78 +41,56 @@ public class AnnounceController {
 
     @GetMapping("/{action}/{passkey}")
     public void announce(@PathVariable String action, @PathVariable String passkey, @RequestParam Map<String, String> gets) throws TrackerException, AnnounceException {
+        if(StringUtils.isEmpty(action)){
+            throw new InvalidAnnounceException("Invalid action");
+        }
+
+        if(StringUtils.isEmpty(passkey)){
+            throw new InvalidAnnounceException("You must re-download the torrent from tracker for seeding.");
+        }
+
         if (!SafeUUID.isDashesStrippedUUID(passkey)) {
-            throw new InvalidPasskeyException();
+            throw new InvalidAnnounceException("Invalid passkey");
         }
 
         checkClient();
 
-        String infoHash = gets.getOrDefault("info_hash", "");
-        String peerId = gets.getOrDefault("peer_id", "");
-        int left = Integer.parseInt(gets.getOrDefault("left", "-1"));
-        var setting = new Setting();
-        var start = timeOfDay();
+
+//        String infoHash = gets.getOrDefault("info_hash", "");
+//        String peerId = gets.getOrDefault("peer_id", "");
+//        int left = Integer.parseInt(gets.getOrDefault("left", "-1"));
+//        var setting = new Setting();
+//        var start = timeOfDay();
         /*var keys = requestUri.split("/");
         if (keys.length < 3 || keys[2].length() != setting.passkeyLength || keys[2].matches("^[a-zA-Z0-9]*$")) {
             throw new TrackerException("Invalid passkey. Re-download torrent!");
         }*/
-        if (infoHash.length() < 4) {
-            var matcher = infoHashPattern.matcher(infoHash);
-            if (matcher.find()) {
-                infoHash = URLDecoder.decode(matcher.group(1), StandardCharsets.UTF_8);
-            }
-        }
-        if (peerId.length() < 4) {
-            var matcher = peerIdPattern.matcher(peerId);
-            if (matcher.matches()) {
-                peerId = URLDecoder.decode(matcher.group(1), StandardCharsets.UTF_8);
-            }
-        }
+//        if (infoHash.length() < 4) {
+//            var matcher = infoHashPattern.matcher(infoHash);
+//            if (matcher.find()) {
+//                infoHash = URLDecoder.decode(matcher.group(1), StandardCharsets.UTF_8);
+//            }
+//        }
+//        if (peerId.length() < 4) {
+//            var matcher = peerIdPattern.matcher(peerId);
+//            if (matcher.matches()) {
+//                peerId = URLDecoder.decode(matcher.group(1), StandardCharsets.UTF_8);
+//            }
+//        }
         if (action.contains("announce")) {
             checkAnnounceFields(gets);
-            peerId = hasheval(peerId, 20, "peer_id");
-            var seeder = left == 0 ? "yes" : "no";
+            String peerId = gets.get("peer_id");
+            //peerId = hasheval(peerId, 20, "peer_id");
+            long left = Long.parseLong(gets.get("left"));
+            boolean seeder = left <= 0;
+            int port = Integer.parseInt(gets.get("port"));
+            AnnounceEventType event = AnnounceEventType.valueOf(gets.get("event"));
+            int numWant = Integer.parseInt(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("numwant"), gets.get("num want"), gets.get("num_want"))).orElse("150"));
+            boolean noPeerId = BooleanUtil.parseBoolean(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("nopeerid"), gets.get("no_peerid"), gets.get("no_peer_id"))).orElse("0"));
+            boolean supportCrypto = BooleanUtil.parseBoolean(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("supportcrypto"), gets.get("support crypto"), gets.get("support_crypto"))).orElse("0"));
+            boolean compact = BooleanUtil.parseBoolean(gets.get("compact"));
+            String peerIp = Optional.ofNullable(MiscUtil.anyNotNull(gets.get("ip"), gets.get("address"), gets.get("ipaddress"), gets.get("ip_address"), gets.get("ip address"))).orElse(request.getRemoteAddr());
 
-            var port = -1;
-            try {
-                port = Integer.parseInt(gets.get("port"));
-                if (port > 0xffff || port < 1)
-                    throw new TrackerException("Invalid port number: " + port + ".");
-            } catch (Throwable e) {
-                throw new TrackerException("Invalid port number.", e);
-            }
-            var ip = getIp();
-            var optionalVars = new String[]{
-                    "numwant",
-                    "compact",
-                    "no_peer_id"
-            };
-            for (var var : optionalVars) {
-                if (gets.containsKey(var) && !gets.get(var).matches("^[0-9]*$")) {
-                    throw new TrackerException("Invalid opt key: " + var + ".");
-                }
-            }
-            var event = "";
-            if (gets.containsKey("event")) {
-                event = gets.get("event");
-                if (!event.matches("^[a-zA-Z]*$")) {
-                    throw new TrackerException("Invalid event.");
-                }
-                var events = Arrays.asList(
-                        "started",
-                        "stopped",
-                        "completed",
-                        "paused");
-                if (!events.contains(event)) {
-                    throw new TrackerException("Invalid event.");
-                }
-            }
-            if (!setting.allowOldProtocols) {
-                if (!gets.containsKey("compact") && (!event.equals("stopped") && !event.equals("completed"))) {
-                    // client has not stopped or completed - should say it supports compact if doing so
-                    throw new TrackerException("Please upgrade or change client.");
-                }
-            }
             var peers = findPeer(infoHash, port, ip);
             if (peers.size() == 0) { // peer not found - insert into database, but only if not event=stopped
                 if (setting.logDebug) {
@@ -167,48 +144,45 @@ public class AnnounceController {
         }
     }
 
-    private void checkAnnounceFields(@NotNull Map<String, String> gets) throws InvalidAnnounceException {
-        if(StringUtils.isEmpty(gets.get("info_hash")))
-            throw new InvalidAnnounceException("Missing param: info_hash");
-        if(StringUtils.isEmpty(gets.get("peer_id")))
-            throw new InvalidAnnounceException("Missing param: peer_id");
-        if(StringUtils.isEmpty(gets.get("port")) || !StringUtils.isNumeric(gets.get("port")))
-            throw new InvalidAnnounceException("Missing/Invalid param: port");
-        if(StringUtils.isEmpty(gets.get("uploaded")) || !StringUtils.isNumeric(gets.get("uploaded")))
-            throw new InvalidAnnounceException("Missing/Invalid param: uploaded");
-        if(StringUtils.isEmpty(gets.get("downloaded")) || !StringUtils.isNumeric(gets.get("downloaded")))
-            throw new InvalidAnnounceException("Missing/Invalid param: downloaded");
-        if(StringUtils.isEmpty(gets.get("left")) || !StringUtils.isNumeric(gets.get("left")))
-            throw new InvalidAnnounceException("Missing/Invalid param: left");
-        if(StringUtils.isEmpty(gets.get("event")))
-            throw new InvalidAnnounceException("Missing param: event");
-        if(AnnounceEventType.fromName(gets.get("event")) == null)
-            throw new InvalidAnnounceException("Invalid param: event");
-    }
-
     private void checkClient() throws InvalidAnnounceException {
         String method = request.getMethod();
         if (!method.equals("GET")) {
-            throw new InvalidAnnounceException("Invalid request method: "+method);
+            throw new InvalidAnnounceException("Invalid request method: " + method);
         }
         String userAgent = request.getHeader("User-Agent");
-        if(blacklistClient.isBanned(userAgent)){
-            throw new InvalidAnnounceException("Banned client: "+userAgent);
+        if (blacklistClient.isBanned(userAgent)) {
+            throw new InvalidAnnounceException("Banned client: " + userAgent);
         }
     }
 
-    public long timeOfDay() {
-        return System.nanoTime() / 1000;
-    }
+    private void checkAnnounceFields(@NotNull Map<String, String> gets) throws InvalidAnnounceException {
+        if (StringUtils.isEmpty(gets.get("info_hash")))
+            throw new InvalidAnnounceException("Missing param: info_hash");
+        if (StringUtils.isEmpty(gets.get("peer_id")))
+            throw new InvalidAnnounceException("Missing param: peer_id");
+        if (StringUtils.isEmpty(gets.get("port")) || !StringUtils.isNumeric(gets.get("port")))
+            throw new InvalidAnnounceException("Missing/Invalid param: port");
+        if (StringUtils.isEmpty(gets.get("uploaded")) || !StringUtils.isNumeric(gets.get("uploaded")))
+            throw new InvalidAnnounceException("Missing/Invalid param: uploaded");
+        if (StringUtils.isEmpty(gets.get("downloaded")) || !StringUtils.isNumeric(gets.get("downloaded")))
+            throw new InvalidAnnounceException("Missing/Invalid param: downloaded");
+        if (StringUtils.isEmpty(gets.get("left")) || !StringUtils.isNumeric(gets.get("left")))
+            throw new InvalidAnnounceException("Missing/Invalid param: left");
+        if (StringUtils.isEmpty(gets.get("event")))
+            throw new InvalidAnnounceException("Missing param: event; Please update or change your client.");
+        if (AnnounceEventType.fromName(gets.get("event")) == null)
+            throw new InvalidAnnounceException("Invalid param: event");
+        String numwant = MiscUtil.anyNotNull(gets.get("numwant"), gets.get("num want"), gets.get("num_want"));
+        if (!StringUtils.isEmpty(numwant) && !StringUtils.isNumeric(numwant)) {
+            throw new InvalidAnnounceException("Invalid optional param: numwant");
+        }
+        if (!StringUtils.isEmpty(gets.get("compact")) && !StringUtils.isNumeric(gets.get("compact"))) {
+            throw new InvalidAnnounceException("Invalid optional param: compact");
+        }
+        if (!StringUtils.isEmpty(gets.get("corrupt")) && !StringUtils.isNumeric(gets.get("corrupt"))) {
+            throw new InvalidAnnounceException("Invalid optional param: corrupt");
+        }
 
-    public String hasheval(String str, int len, String name) {
-        // TODO
-        return "";
-    }
-
-    public String getIp() {
-        // TODO
-        return "0.0.0.0";
     }
 
     public Set<Object> findPeer(String infoHash, int port, String ip) {
@@ -234,8 +208,22 @@ public class AnnounceController {
         // TODO
     }
 
+    public long timeOfDay() {
+        return System.nanoTime() / 1000;
+    }
+
     public void sendBencode() {
         // TODO
+    }
+
+    public String hasheval(String str, int len, String name) {
+        // TODO
+        return "";
+    }
+
+    public String getIp() {
+        // TODO
+        return "0.0.0.0";
     }
 
     public boolean connectable(String ip, int port) {
