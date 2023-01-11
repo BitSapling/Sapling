ee12b58bf132139fcdacaa88ff16df5748f04b6d
Ghost_chu <ghostchu@111.com>
2023-01-11 22:56:55 +0800

    refactor: refactoring the controller dor announcing

--- src/main/java/com/github/bitsapling/sapling/controller/announce/AnnounceController.java
+++ src/main/java/com/github/bitsapling/sapling/controller/announce/AnnounceController.java
@@ -1,18 +1,24 @@
 package com.github.bitsapling.sapling.controller.announce;
 
+import com.dampcake.bencode.Bencode;
+import com.github.bitsapling.sapling.entity.Peer;
 import com.github.bitsapling.sapling.exception.BrowserReadableAnnounceException;
 import com.github.bitsapling.sapling.exception.FixedAnnounceException;
 import com.github.bitsapling.sapling.exception.InvalidAnnounceException;
 import com.github.bitsapling.sapling.exception.TrackerException;
 import com.github.bitsapling.sapling.service.BlacklistClientService;
+import com.github.bitsapling.sapling.service.PeerService;
 import com.github.bitsapling.sapling.type.AnnounceEventType;
+import com.github.bitsapling.sapling.type.IPFormatRequirement;
 import com.github.bitsapling.sapling.util.BooleanUtil;
 import com.github.bitsapling.sapling.util.MiscUtil;
 import com.github.bitsapling.sapling.util.SafeUUID;
+import com.google.common.hash.Hashing;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,6 +26,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -34,13 +41,16 @@ public class AnnounceController {
     private static final Random random = new Random();
     private static final Pattern infoHashPattern = Pattern.compile("info_hash=(.*?)($|&)");
     private static final Pattern peerIdPattern = Pattern.compile("peer_id=(.*?)&");
-    private final HttpServletRequest request;
-    private final BlacklistClientService blacklistClient;
-
-    public AnnounceController(@Autowired HttpServletRequest request, @Autowired BlacklistClientService blacklistClient) {
-        this.request = request;
-        this.blacklistClient = blacklistClient;
-    }
+    private static final Bencode BITTORRENT_STANDARD_BENCODE_ENCODER = new Bencode(StandardCharsets.ISO_8859_1);
+    private static final InetAddressValidator ipValidator = InetAddressValidator.getInstance();
+    private static final int MIN_INTERVAL = 60 * 60 * 15;
+    private static final int MAX_INTERVAL = 60 * 60 * 45;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private BlacklistClientService blacklistClientService;
+    @Autowired
+    private PeerService peerService;
 
     @GetMapping("/scrape/{passkey}")
     public void scrape(@PathVariable String passkey, @RequestParam List<String> infoHashes) {
@@ -48,7 +58,7 @@ public class AnnounceController {
     }
 
     @GetMapping("/announce/{passkey}")
-    public void announce(@PathVariable String passkey, @RequestParam Map<String, String> gets) throws TrackerException, FixedAnnounceException, BrowserReadableAnnounceException {
+    public String announce(@PathVariable String passkey, @RequestParam Map<String, String> gets) throws TrackerException, FixedAnnounceException, BrowserReadableAnnounceException {
         long start = timeOfDay();
 
         if (StringUtils.isEmpty(passkey)) {
@@ -58,71 +68,78 @@ public class AnnounceController {
         if (!SafeUUID.isDashesStrippedUUID(passkey)) {
             throw new InvalidAnnounceException("Invalid passkey.");
         }
-
         checkClient();
-
-        Setting setting = getSettings();
-
         checkAnnounceFields(gets);
         String peerId = gets.get("peer_id");
-        //peerId = hasheval(peerId, 20, "peer_id");
+        String peerIdHash = Hashing.murmur3_128().hashString(peerId, StandardCharsets.UTF_8).toString();
         long left = Long.parseLong(gets.get("left"));
         boolean seeder = left == 0;
         int port = Integer.parseInt(gets.get("port"));
         AnnounceEventType event = AnnounceEventType.valueOf(gets.get("event"));
         int numWant = Integer.parseInt(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("numwant"), gets.get("num want"), gets.get("num_want"))).orElse("150"));
+        numWant = Math.min(numWant, 300);
         boolean noPeerId = BooleanUtil.parseBoolean(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("nopeerid"), gets.get("no_peerid"), gets.get("no_peer_id"))).orElse("0"));
         boolean supportCrypto = BooleanUtil.parseBoolean(Optional.ofNullable(MiscUtil.anyNotNull(gets.get("supportcrypto"), gets.get("support crypto"), gets.get("support_crypto"))).orElse("0"));
         boolean compact = BooleanUtil.parseBoolean(gets.get("compact"));
         String peerIp = Optional.ofNullable(MiscUtil.anyNotNull(gets.get("ip"), gets.get("address"), gets.get("ipaddress"), gets.get("ip_address"), gets.get("ip address"))).orElse(request.getRemoteAddr());
         String infoHash = gets.get("info_hash");
 
-        var peers = findPeer(infoHash, port, peerIp);
-        if (peers.size() == 0) { // peer not found - insert into database, but only if not event=stopped
-            if (setting.logDebug) {
-                debuglog("announce: peer not found!");
-            }
-            if (event == AnnounceEventType.STOPPED) {
-                throw new TrackerException("Client sent stop, but peer not found!");
-            }
-            var user = gatherUser(passkey).orElseThrow(() -> new TrackerException("Permission denied."));
-            var torrent = gatherTorrent(infoHash).orElseThrow(() -> new TrackerException("Torrent does not exist on this tracker."));
+        // TODO check whether user have permission to download
 
-            var timesCompleted = false;
-            var timesStarted = false;
-            var timesUpdated = false;
-            if (event == AnnounceEventType.COMPLETED && left == 0) {
-                timesCompleted = true;
-            } else if (event == AnnounceEventType.STARTED) {// && left == 0) { //TODO: start size check
-                timesStarted = true;
-            } else {
-                timesUpdated = true;
-            }
-            // TODO: update torrent status
-            if (seeder) {
-                // TODO: update seeder status
-            } else {
-                // TODO: update leecher status
-            }
-        } else if (peers.size() == 1) {
-            if (setting.rateLimitation) {
-                // TODO: rate detection
-            }
-            if (setting.registerStats) { // TODO: check if there is changes
+        return generatePeersResponse(peerIdHash, infoHash, numWant, compact);
 
-            }
-            give_peers();
-        } else {
-            // we hit multiple? but that's IMPOSSIBLE! ;)
-            if (setting.logDebug) {
-                debuglog("announce: got multiple targets in peer table!");
-            }
-            throw new TrackerException("Got multiple targets in peer table!");
-        }
 
-        if (setting.timeMe && setting.logDebug) {
-            debuglog("announce: " + (timeOfDay() - start) + " us");
-        }
+//
+//        if (peers.size() == 0) { // peer not found - insert into database, but only if not event=stopped
+//            if (setting.logDebug) {
+//                debuglog("announce: peer not found!");
+//            }
+//            if (event == AnnounceEventType.STOPPED) {
+//                throw new TrackerException("Client sent stop, but peer not found!");
+//            }
+//            var user = gatherUser(passkey).orElseThrow(() -> new TrackerException("Permission denied."));
+//            var torrent = gatherTorrent(infoHash).orElseThrow(() -> new TrackerException("Torrent does not exist on this tracker."));
+//
+//            var timesCompleted = false;
+//            var timesStarted = false;
+//            var timesUpdated = false;
+//            if (event == AnnounceEventType.COMPLETED && left == 0) {
+//                timesCompleted = true;
+//            } else if (event == AnnounceEventType.STARTED) {// && left == 0) { //TODO: start size check
+//                timesStarted = true;
+//            } else {
+//                timesUpdated = true;
+//            }
+//            // TODO: update torrent status
+//            if (seeder) {
+//                // TODO: update seeder status
+//            } else {
+//                // TODO: update leecher status
+//            }
+//        } else if (peers.size() == 1) {
+//            if (setting.rateLimitation) {
+//                // TODO: rate detection
+//            }
+//            if (setting.registerStats) { // TODO: check if there is changes
+//
+//            }
+//            give_peers();
+//        } else {
+//            // we hit multiple? but that's IMPOSSIBLE! ;)
+//            if (setting.logDebug) {
+//                debuglog("announce: got multiple targets in peer table!");
+//            }
+//            throw new TrackerException("Got multiple targets in peer table!");
+//        }
+//
+//        if (setting.timeMe && setting.logDebug) {
+//            debuglog("announce: " + (timeOfDay() - start) + " us");
+//        }
+    }
+
+    public long timeOfDay() {
+        return System.nanoTime() / 1000;
+
     }
 
     private void checkClient() throws FixedAnnounceException, BrowserReadableAnnounceException {
@@ -163,59 +180,66 @@ public class AnnounceController {
 
     }
 
-    public Set<Object> findPeer(String infoHash, int port, String ip) {
-        // TODO
-        return new HashSet<>();
+    @NotNull
+    private String generatePeersResponse(String peerIdHash, String infoHash, int numWant, boolean compact) {
+        if (compact) {
+            return generatePeersResponseCompat(peerIdHash, infoHash, numWant);
+        } else {
+            return generatePeersResponseNonCompat(peerIdHash, infoHash, numWant, compact);
+        }
     }
 
-    public void debuglog(String log) {
-        // TODO
+    @NotNull
+    private String generatePeersResponseCompat(String peerIdHash, String infoHash, int numWant) {
+        // TODO: What the fuck
+        // http://bittorrent.org/beps/bep_0023.html
+        Map<IPFormatRequirement, List<Map<String, String>>> peers = gatherPeers(peerIdHash, infoHash, numWant, true);
+        Map<String, Object> dict = new HashMap<>();
+        dict.put("interval", randomInterval());
+        dict.put("min interval", randomInterval());
+        dict.put("peers", peers.get(IPFormatRequirement.IPV4));
+        dict.put("peers6", peers.get(IPFormatRequirement.IPV6));
+        return new String(BITTORRENT_STANDARD_BENCODE_ENCODER.encode(dict), BITTORRENT_STANDARD_BENCODE_ENCODER.getCharset());
     }
 
-    public Optional<Object> gatherUser(String passkey) {
-        // TODO
-        return Optional.empty();
+    @NotNull
+    private String generatePeersResponseNonCompat(String peerIdHash, String infoHash, int numWant, boolean noPeerId) {
+        PeerResult peers = gatherPeers(peerIdHash, infoHash, numWant);
+        List<Map<String,String>> peerList = new ArrayList<>();
+        List<Peer> allPeers = new ArrayList<>(peers.peers());
+        allPeers.addAll(peers.peers6());
+        for (Peer peer : allPeers) {
+            Map<String, String> peerMap = new HashMap<>();
+            if (!noPeerId) {
+                peerMap.put("peer id", peer.getPeerId());
+            }
+            peerMap.put("ip", peer.getIp());
+            peerMap.put("port", String.valueOf(peer.getPort()));
+            peerList.add(peerMap);
+        }
+        Map<String, Object> dict = new HashMap<>();
+        dict.put("interval", randomInterval());
+        dict.put("peers", peerList);
+        return new String(BITTORRENT_STANDARD_BENCODE_ENCODER.encode(dict), BITTORRENT_STANDARD_BENCODE_ENCODER.getCharset());
     }
 
-    public Optional<Object> gatherTorrent(String infoHash) {
-        // TODO
-        return Optional.empty();
+    @NotNull
+    private PeerResult gatherPeers(@NotNull String peerIdHashExclude, @NotNull String infoHash, int numWant) {
+        List<Peer> torrentPeers = peerService.fetchPeers(infoHash, numWant);
+        // Remove itself
+        torrentPeers.removeIf(peer -> peer.getPeerIdHash().equals(peerIdHashExclude));
+        List<Peer> v4 = torrentPeers.stream().filter(peer->ipValidator.isValidInet4Address(peer.getIp())).toList();
+        List<Peer> v6 = torrentPeers.stream().filter(peer->ipValidator.isValidInet6Address(peer.getIp())).toList();
+        long completed = torrentPeers.stream().filter(Peer::isSeeder).count();
+        long incompleted = torrentPeers.size() - completed;
+        return new PeerResult(v4,v6,completed,incompleted);
     }
 
-    public void give_peers() {
-        // TODO
+    private String randomInterval() {
+        return String.valueOf(random.nextInt(MIN_INTERVAL, MAX_INTERVAL));
     }
 
-    public long timeOfDay() {
-        return System.nanoTime() / 1000;
-
-    }
-
-    public void sendBencode() {
-        // TODO
-    }
-
-    public String hasheval(String str, int len, String name) {
-        // TODO
-        return "";
-    }
-
-    public String getIp() {
-        // TODO
-        return "0.0.0.0";
-    }
-
-    public boolean connectable(String ip, int port) {
-        return false;
-    }
-
-    public long time() {
-        return System.currentTimeMillis() / 1000;
-    }
-
-    public Setting getSettings() {
-        // TODO
-        return new Setting();
+    record PeerResult(@NotNull List<Peer> peers, List<Peer> peers6, long complete, long incomplete) {
     }
 
     @Data
