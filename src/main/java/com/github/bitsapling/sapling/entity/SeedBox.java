562e3b5a844b6b2952cb015fd1a0b9ce7a8ada26
Ghost_chu <ghostchu@111.com>
2023-01-12 18:30:43 +0800

    feat: Seedbox table

---
+++ src/main/java/com/github/bitsapling/sapling/entity/SeedBox.java
@@ -0,0 +1,24 @@
+package com.github.bitsapling.sapling.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "seedbox",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"infoHash"})
+        }
+)
+@Data
+public class SeedBox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+    @Column(name = "address", nullable = false)
+    private String address;
+    @Column(name = "download_multipler")
+    private Double downloadMultiplier;
+    @Column(name = "upload_multipler")
+    private Double uploadMultiplier;
+}
