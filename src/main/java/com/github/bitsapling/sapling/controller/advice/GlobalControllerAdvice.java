c7f88ce85a3786a7fd2887f384856401571fc693
Ghost_chu <ghostchu@111.com>
2023-02-04 21:14:20 +0800

    Replace with Bencode Util

--- src/main/java/com/github/bitsapling/sapling/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/com/github/bitsapling/sapling/controller/advice/GlobalControllerAdvice.java
@@ -1,22 +1,20 @@
 package com.github.bitsapling.sapling.controller.advice;
 
-import com.dampcake.bencode.Bencode;
 import com.github.bitsapling.sapling.exception.BrowserReadableAnnounceException;
 import com.github.bitsapling.sapling.exception.FixedAnnounceException;
 import com.github.bitsapling.sapling.exception.RetryableAnnounceException;
+import com.github.bitsapling.sapling.util.BencodeUtil;
 import com.github.bitsapling.sapling.util.ClassUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
-    private static final Bencode BITTORRENT_STANDARD_BENCODE_ENCODER = new Bencode(StandardCharsets.ISO_8859_1);
     @Autowired
     private ClassUtil classUtil;
 
@@ -26,7 +24,7 @@ public class GlobalControllerAdvice {
         Map<String, String> dict = new HashMap<>();
         dict.put("failure reason", classUtil.getClassSimpleName(exception.getClass()) + ": " + exception.getMessage());
         dict.put("retry in", "never");
-        return new String(BITTORRENT_STANDARD_BENCODE_ENCODER.encode(dict), BITTORRENT_STANDARD_BENCODE_ENCODER.getCharset());
+        return new String(BencodeUtil.bittorrent().encode(dict), BencodeUtil.bittorrent().getCharset());
     }
 
     @ExceptionHandler(value = RetryableAnnounceException.class)
@@ -35,7 +33,7 @@ public class GlobalControllerAdvice {
         Map<String, String> dict = new HashMap<>();
         dict.put("failure reason", classUtil.getClassSimpleName(exception.getClass()) + ": " + exception.getMessage());
         dict.put("retry in", String.valueOf(exception.getRetryIn()));
-        return new String(BITTORRENT_STANDARD_BENCODE_ENCODER.encode(dict), BITTORRENT_STANDARD_BENCODE_ENCODER.getCharset());
+        return new String(BencodeUtil.bittorrent().encode(dict), BencodeUtil.bittorrent().getCharset());
     }
 
     @ExceptionHandler(value = BrowserReadableAnnounceException.class)
