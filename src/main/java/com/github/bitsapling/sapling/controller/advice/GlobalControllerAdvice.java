6fae715edb01e2e5939fd3cdddb82914b3581d0e
Ghost_chu <ghostchu@111.com>
2023-02-04 21:18:08 +0800

    Use ResponseEntity for exception handler

--- src/main/java/com/github/bitsapling/sapling/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/com/github/bitsapling/sapling/controller/advice/GlobalControllerAdvice.java
@@ -6,6 +6,7 @@ import com.github.bitsapling.sapling.exception.RetryableAnnounceException;
 import com.github.bitsapling.sapling.util.BencodeUtil;
 import com.github.bitsapling.sapling.util.ClassUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,25 +21,28 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = FixedAnnounceException.class)
     @ResponseBody
-    public String announceExceptionHandler(FixedAnnounceException exception) {
+    public ResponseEntity<String> announceExceptionHandler(FixedAnnounceException exception) {
         Map<String, String> dict = new HashMap<>();
         dict.put("failure reason", classUtil.getClassSimpleName(exception.getClass()) + ": " + exception.getMessage());
         dict.put("retry in", "never");
-        return new String(BencodeUtil.bittorrent().encode(dict), BencodeUtil.bittorrent().getCharset());
+        return ResponseEntity.internalServerError()
+                .body(BencodeUtil.convertToString(BencodeUtil.bittorrent().encode(dict)));
     }
 
     @ExceptionHandler(value = RetryableAnnounceException.class)
     @ResponseBody
-    public String announceExceptionHandler(RetryableAnnounceException exception) {
+    public ResponseEntity<String> announceExceptionHandler(RetryableAnnounceException exception) {
         Map<String, String> dict = new HashMap<>();
         dict.put("failure reason", classUtil.getClassSimpleName(exception.getClass()) + ": " + exception.getMessage());
         dict.put("retry in", String.valueOf(exception.getRetryIn()));
-        return new String(BencodeUtil.bittorrent().encode(dict), BencodeUtil.bittorrent().getCharset());
+        return ResponseEntity.internalServerError()
+                .body(BencodeUtil.convertToString(BencodeUtil.bittorrent().encode(dict)));
     }
 
     @ExceptionHandler(value = BrowserReadableAnnounceException.class)
     @ResponseBody
-    public String announceExceptionHandler(BrowserReadableAnnounceException exception) {
-        return exception.getMessage();
+    public ResponseEntity<String> announceExceptionHandler(BrowserReadableAnnounceException exception) {
+        return ResponseEntity.internalServerError()
+                .body(exception.getClass().getName()+": "+exception.getMessage());
     }
 }
