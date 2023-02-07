f276331bce8c501cd111cf8469d2bb65a4e16cdb
Ghost_chu <ghostchu@111.com>
2023-02-07 21:35:46 +0800

    fix: CloudFlare IP detecting

--- src/main/java/com/github/bitsapling/sapling/util/IPUtil.java
+++ src/main/java/com/github/bitsapling/sapling/util/IPUtil.java
@@ -1,14 +1,15 @@
 package com.github.bitsapling.sapling.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-
+@Slf4j
 public class IPUtil {
     @NotNull
     public static String getRequestIp(@NotNull HttpServletRequest request) {
-        String realIp = request.getHeader("X-REAL-IP");
+        String realIp = request.getHeader("CF-CONNECTING-IP");
         if (realIp == null)
-            realIp = request.getHeader("CF-Connecting-IP");
+            realIp = request.getHeader("X-REAL-IP");
         if (realIp == null)
             realIp = request.getHeader("X-FORWARDED-FOR");
         if (realIp == null)
