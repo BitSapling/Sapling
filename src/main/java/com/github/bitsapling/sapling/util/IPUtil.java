f0067e171bb17c36ee2e0ecaab67daf19b01e03a
Ghost_chu <ghostchu@111.com>
2023-02-07 21:23:10 +0800

    fix: CloudFlare IP recognize

--- src/main/java/com/github/bitsapling/sapling/util/IPUtil.java
+++ src/main/java/com/github/bitsapling/sapling/util/IPUtil.java
@@ -5,11 +5,13 @@ import org.jetbrains.annotations.NotNull;
 
 public class IPUtil {
     @NotNull
-    public static String getRequestIp(@NotNull HttpServletRequest request){
+    public static String getRequestIp(@NotNull HttpServletRequest request) {
         String realIp = request.getHeader("X-REAL-IP");
-        if(realIp == null)
-           realIp = request.getHeader("X-FORWARDED-FOR");
-        if(realIp == null)
+        if (realIp == null)
+            realIp = request.getHeader("CF-Connecting-IP");
+        if (realIp == null)
+            realIp = request.getHeader("X-FORWARDED-FOR");
+        if (realIp == null)
             realIp = request.getRemoteAddr();
         return realIp;
     }
