b34a337694e91177f6c48f13e18caa3f03a1ae2d
Ghost_chu <ghostchu@111.com>
2023-02-10 02:47:51 +0800

    feat: 修复搜索

--- src/main/java/com/github/bitsapling/sapling/repository/PromotionPolicyRepository.java
+++ src/main/java/com/github/bitsapling/sapling/repository/PromotionPolicyRepository.java
@@ -4,7 +4,9 @@ import com.github.bitsapling.sapling.entity.PromotionPolicy;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PromotionPolicyRepository extends CrudRepository<PromotionPolicy, Long> {
-
+    Optional<PromotionPolicy> findPromotionPolicyBySlug(String slug);
 }
