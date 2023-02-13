652d582d7c6b14ab92ae04cc554e58653cdc3513
Ghost_chu <ghostchu@111.com>
2023-02-13 20:40:09 +0800

    fix: 修复传输历史活跃检查问题

--- src/main/java/com/github/bitsapling/sapling/service/TransferHistoryService.java
+++ src/main/java/com/github/bitsapling/sapling/service/TransferHistoryService.java
@@ -1,5 +1,6 @@
 package com.github.bitsapling.sapling.service;
 
+import com.github.bitsapling.sapling.config.TrackerConfig;
 import com.github.bitsapling.sapling.entity.Torrent;
 import com.github.bitsapling.sapling.entity.TransferHistory;
 import com.github.bitsapling.sapling.entity.User;
@@ -22,6 +23,8 @@ public class TransferHistoryService {
     private TransferHistoryRepository repository;
     @Autowired
     private PeerService peerService;
+    @Autowired
+    private SettingService settingService;
 
     @Nullable
     public TransferHistory getTransferHistory(@NotNull User user, @NotNull Torrent torrent) {
@@ -35,32 +38,45 @@ public class TransferHistoryService {
 
     @NotNull
     public List<TransferHistory> getTransferHistoryActive(@NotNull Torrent torrent) {
-        Timestamp timestamp = Timestamp.from(Instant.now().minus(2, ChronoUnit.HOURS));
+        TrackerConfig config = settingService.get(TrackerConfig.getConfigKey(), TrackerConfig.class);
+        Timestamp timestamp = Timestamp.from(Instant.now().minus(config.getTorrentIntervalMax() + 15000, ChronoUnit.MILLIS));
         return repository.findAllByTorrentAndUpdatedAtAfterOrderByUpdatedAt(torrent, timestamp);
     }
 
     @NotNull
     public PeerStatus getPeerStatus(@NotNull Torrent torrent) {
-        List<TransferHistory> histories = getTransferHistoryActive(torrent);
+        TrackerConfig config = settingService.get(TrackerConfig.getConfigKey(), TrackerConfig.class);
+        List<TransferHistory> histories = getTransferHistory(torrent);
         int complete = 0;
         int incomplete = 0;
         int downloaders = 0;
         int downloaded = 0;
         for (TransferHistory history : histories) {
-            if (history.getLastEvent() == AnnounceEventType.PAUSED) { // 部分做种
-                downloaders++;
-                continue;
-            }
-            if (history.isHaveCompleteHistory()) { // 曾经完成过下载
-                downloaded++;
-            }
-            if (history.getLeft() == 0) { // 下载报告已完成
-                complete++;
-            } else { // 下载还未完成 (不和 haveCompleteHistory 合并，因为可以下载多次）
-                incomplete++;
+            if (isTransferActive(history, config)) {
+                if (history.getLastEvent() == AnnounceEventType.PAUSED) { // 部分做种
+                    downloaders++;
+                    continue;
+                }
+                if (history.isHaveCompleteHistory()) { // 曾经完成过下载
+                    downloaded++;
+                }
+                if (history.getLeft() == 0) { // 下载报告已完成
+                    complete++;
+                } else { // 下载还未完成 (不和 haveCompleteHistory 合并，因为可以下载多次）
+                    incomplete++;
+                }
+            } else {
+                if (history.getLeft() == 0) { // 下载报告已完成
+                    complete++;
+                }
             }
         }
         return new PeerStatus(complete, incomplete, downloaded, downloaders);
+    }
+
+    private boolean isTransferActive(TransferHistory history, TrackerConfig config) {
+        Timestamp timestamp = Timestamp.from(Instant.now().minus(config.getTorrentIntervalMax() + 15000, ChronoUnit.MILLIS));
+        return history.getUpdatedAt().after(timestamp);
     }
 
     @NotNull
