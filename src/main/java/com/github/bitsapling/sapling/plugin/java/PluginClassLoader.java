12121e02bea3a8021ef3b4b87599986a8bde1637
Baleine <lj2000lj@126.com>
2023-06-11 14:31:26 +1000

    feat: class loader for plugins

--- src/main/java/com/github/bitsapling/sapling/plugin/java/PluginClassLoader.java
+++ src/main/java/com/github/bitsapling/sapling/plugin/java/PluginClassLoader.java
@@ -2,14 +2,26 @@ package com.github.bitsapling.sapling.plugin.java;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Set;
 
 public class PluginClassLoader extends URLClassLoader {
-    private static final String[] BLACKLISTED_CLASS_PREFIXES = {
+    private static final Set<String> BLACKLISTED_CLASS_PREFIXES = Set.of(new String[]{
             "com.github.bitsapling.sapling"
-    };
+    });
 
     public PluginClassLoader(String name, URL[] urls, ClassLoader parent) {
         super(name, urls, parent);
+    }
+
+    @Override
+    public Class<?> loadClass(String className) throws ClassNotFoundException {
+        if (className.startsWith("java.")) {
+            return findSystemClass(className);
+        }
+        if (BLACKLISTED_CLASS_PREFIXES.stream().anyMatch(className::startsWith)) {
+            return getParent().loadClass(className);
+        }
+        return super.loadClass(className);
     }
 
 }
