98fd67345f396f0ada03d024b1f5519337c7a6f0
Ghost_chu <ghostchu@111.com>
2023-06-14 00:07:32 +0800

    clean up

--- src/main/java/com/github/bitsapling/sapling/module/tracker/handle/AnnounceTask.java
+++ src/main/java/com/github/bitsapling/sapling/module/tracker/handle/AnnounceTask.java
@@ -16,12 +16,10 @@ public class AnnounceTask {
     private final long left;
     private final @NotNull AnnounceEventType event;
     private final int numWant;
-    private final long userId;
     private final boolean compact;
     private final boolean noPeerId;
     private final boolean supportCrypto;
     private final int redundant;
     private final @NotNull String userAgent;
     private final @NotNull String passKey;
-    private final long torrentId;
 }
