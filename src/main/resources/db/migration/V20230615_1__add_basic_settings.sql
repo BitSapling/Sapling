bf9d8f8da2f808c15959a26ed31656de04e939ee
Ghost_chu <ghostchu@111.com>
2023-06-15 19:24:54 +0800

    Add some settings into settings table

---
+++ src/main/resources/db/migration/V20230615_1__add_basic_settings.sql
@@ -0,0 +1,24 @@
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (1, 'general.site-name', 'Another Sapling Tracker');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (2, 'general.site-url', 'http://localhost:8080');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (3, 'general.site-description', 'Upload and share your files! Powered by Sapling Tracker.');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (4, 'general.site-keywords', 'sapling,bittorrent,tracker');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (5, 'tracker.prefix', 'Sapling');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (6, 'tracker.min-announce-interval', '56000');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (7, 'tracker.max-announce-interval', '162000');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (8, 'tracker.allow-upload-bittorrent-v1', 'true');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (9, 'tracker.allow-upload-bittorrent-v2', 'true');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (10, 'tracker.allow-bittorrent-mixed', 'true');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (11, 'security.max-login-attempts', '10');
+INSERT INTO `settings` (`id`, `key`, `value`)
+VALUES (12, 'security.login-attempt-ban', '900');
