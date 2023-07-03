eb147d2a190c55c4b2cf19f4bbd2535ee08757e3
Ghost_chu <ghostchu@111.com>
2023-07-04 00:53:34 +0800

    feat: UA 检查，Peer Id 也应使用此工具

--- src/main/java/com/github/bitsapling/sapling/module/uacontrol/BtClientUAService.java
+++ src/main/java/com/github/bitsapling/sapling/module/uacontrol/BtClientUAService.java
@@ -1,6 +1,7 @@
 package com.github.bitsapling.sapling.module.uacontrol;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.bitsapling.sapling.module.common.CommonService;
@@ -12,10 +13,32 @@ import java.util.List;
 @Service
 public class BtClientUAService extends ServiceImpl<BtClientUAMapper, BtClientUA> implements CommonService<BtClientUA> {
 
+    @SuppressWarnings("SameParameterValue")
     @NotNull List<BtClientUA> getAllRules(boolean onlyEnabled) {
         LambdaQueryWrapper<BtClientUA> wrapper = Wrappers
                 .lambdaQuery(BtClientUA.class)
                 .eq(onlyEnabled, BtClientUA::getEnabled, true);
         return baseMapper.selectList(wrapper);
+    }
+
+    public boolean isAllowedClient(String clientUA) {
+        if (StringUtils.isEmpty(clientUA)) return false;
+        List<BtClientUA> rules = getAllRules(true);
+        boolean allowed = false;
+        for (BtClientUA rule : rules) {
+            boolean rulePassed = switch (rule.getMatchType()) {
+                case EQUALS -> clientUA.equals(rule.getUserAgent());
+                case CONTAINS -> clientUA.contains(rule.getUserAgent());
+                case NOT_CONTAINS -> !clientUA.contains(rule.getUserAgent());
+                case START_WITH -> clientUA.startsWith(rule.getUserAgent());
+                case END_WITH -> clientUA.endsWith(rule.getUserAgent());
+                case REGEX -> clientUA.matches(rule.getUserAgent());
+            };
+            if (rulePassed) {
+                allowed = true;
+                break;
+            }
+        }
+        return allowed;
     }
 }
