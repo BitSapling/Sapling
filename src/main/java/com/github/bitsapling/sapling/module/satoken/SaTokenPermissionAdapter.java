2075e344ce8b86be29dc0bc5f7635c07e8d2f1a5
Ghost_chu <ghostchu@111.com>
2023-07-04 03:02:30 +0800

    feat: 为 Sa-Token 添加缓存

--- src/main/java/com/github/bitsapling/sapling/module/satoken/SaTokenPermissionAdapter.java
+++ src/main/java/com/github/bitsapling/sapling/module/satoken/SaTokenPermissionAdapter.java
@@ -1,15 +1,20 @@
 package com.github.bitsapling.sapling.module.satoken;
 
+import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.session.SaSessionCustomUtil;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpUtil;
 import com.github.bitsapling.sapling.module.group.Group;
 import com.github.bitsapling.sapling.module.group.GroupService;
 import com.github.bitsapling.sapling.module.permission.Permission;
 import com.github.bitsapling.sapling.module.permission.PermissionService;
 import com.github.bitsapling.sapling.module.user.User;
 import com.github.bitsapling.sapling.module.user.UserService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,18 +29,36 @@ public class SaTokenPermissionAdapter implements StpInterface {
 
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
+        List<String> permissionList = new ArrayList<>();
+        for (String roleId : getRoleList(loginId, loginType)) {
+            SaSession roleSession = SaSessionCustomUtil.getSessionById("role-" + roleId);
+            List<String> list = roleSession.get("Permission_List", () -> readPermissionsFromDatabase(loginId));
+            permissionList.addAll(list);
+        }
+        return permissionList;
+    }
+
+
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        SaSession session = StpUtil.getSessionByLoginId(loginId);
+        return session.get("Role_List", () -> readRolesFromDatabase(loginId));
+    }
+
+
+    private List<String> readRolesFromDatabase(@NotNull Object loginId) {
+        User user = userService.getUser(loginId);
+        if (user == null) return Collections.emptyList();
+        Group group = groupService.getById(user.getGroup());
+        return List.of(group.getName());
+    }
+
+    @NotNull
+    private List<String> readPermissionsFromDatabase(@NotNull Object loginId) {
         User user = userService.getUser(loginId);
         if (user == null) return Collections.emptyList();
         Group group = groupService.getById(user.getGroup());
         if (group == null) return Collections.emptyList();
         return permissionService.getPermissionByGroup(group.getId()).stream().map(Permission::getPermission).toList();
-    }
-
-    @Override
-    public List<String> getRoleList(Object loginId, String loginType) {
-        User user = userService.getUser(loginId);
-        if (user == null) return Collections.emptyList();
-        Group group = groupService.getById(user.getGroup());
-        return List.of(group.getName());
     }
 }
