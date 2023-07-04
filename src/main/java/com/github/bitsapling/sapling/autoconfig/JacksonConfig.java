e68bce2548d9bc9e85f783cc8d9bc954d8b76340
Ghost_chu <ghostchu@111.com>
2023-07-05 02:26:52 +0800

    fix: LocalDateTime 序列化不正确

--- src/main/java/com/github/bitsapling/sapling/autoconfig/JacksonConfig.java
+++ src/main/java/com/github/bitsapling/sapling/autoconfig/JacksonConfig.java
@@ -3,13 +3,32 @@ package com.github.bitsapling.sapling.autoconfig;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class JacksonConfig {
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
     @Bean
     @Primary
     public ObjectMapper primaryObjectMapper() {
@@ -17,6 +36,26 @@ public class JacksonConfig {
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .findAndAddModules()
                 .addModule(new JavaTimeModule())
+                .addModule(new SimpleModule()
+                        .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                        .addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                        .addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)))
+                        .addSerializer(BigInteger.class, ToStringSerializer.instance)
+                        .addSerializer(Long.class, ToStringSerializer.instance)
+                        .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT)))
+                        .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
+                        .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT))))
                 .build();
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizeLocalDateTimeFormat() {
+        return jacksonObjectMapperBuilder -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTimeDeserializer deserializer = new LocalDateTimeDeserializer(formatter);
+            LocalDateTimeSerializer serializer = new LocalDateTimeSerializer(formatter);
+            jacksonObjectMapperBuilder.serializerByType(LocalDateTime.class, serializer);
+            jacksonObjectMapperBuilder.deserializerByType(LocalDateTime.class, deserializer);
+        };
     }
 }
